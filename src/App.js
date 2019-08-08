80277a2018f02083970f7865a4fa4f0e344f0c29
Mario <rozic.mario@hotmail.com>
2019-08-08 19:05:27 +0200

    :sparkles: Split into components

--- src/App.js
+++ src/App.js
@@ -1,86 +1,66 @@
 import React, { Component } from "react";
 import "./App.css";
-import Song from "./fire_bow.mp3";
+
+import Timer from "./timer";
 
 class App extends Component {
   state = {
-    start: 1,
-    rest: 1,
-    isResting: false,
-    isSettings: true
+    isSettings: true,
+    workoutTime: "",
+    restTime: ""
   };
 
-  componentDidMount = () => {
-    // this.startStart();
+  handlerInput = event => {
+    console.log("Input handler", event.target.value, event.target.name);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
 
-  stopTimer = () => {
-    console.log("Claer Start");
-    clearInterval(this.timerInterval);
-  };
-
-  startStart = () => {
-    this.timerInterval = setInterval(() => {
-      console.log("Start");
-      this.setState({ start: this.state.start + 1 }, () => {
-        if (this.state.start === 46) {
-          console.log("Claer Start");
-          clearInterval(this.timerInterval);
-          this.setState({ isResting: true, start: 1 });
-          this.startRest();
-        }
-      });
-    }, 1000);
-  };
-
-  startRest = () => {
-    this.restInterval = setInterval(() => {
-      console.log("Rest");
-      this.setState({ rest: this.state.rest + 1 }, () => {
-        if (this.state.rest === 16) {
-          console.log("Claer Rest");
-          clearInterval(this.restInterval);
-          this.setState({ isResting: false, rest: 1 });
-          this.startStart();
-        }
-      });
-    }, 1000);
+  handlerButton = () => {
+    console.log("Button handler");
+    this.setState({ isSettings: false });
   };
 
   render() {
-    let { start, rest, isResting, isSettings } = this.state;
+    let { isSettings, workoutTime, restTime } = this.state;
 
     let settingScreen = (
       <div className="Screen blue">
         <div className="workout">
           <span>Workout time</span>
-          <input type="number" />
+          <input
+            name="workoutTime"
+            type="number"
+            placeholder="sec"
+            value={workoutTime}
+            onChange={this.handlerInput}
+          />
         </div>
         <div className="rest">
           <span>Rest time</span>
-          <input type="number" />
+          <input
+            name="restTime"
+            type="number"
+            placeholder="sec"
+            value={restTime}
+            onChange={this.handlerInput}
+          />
         </div>
-        <div className="button">GO</div>
+        <div className="button" onClick={this.handlerButton}>
+          GO
+        </div>
       </div>
     );
 
-    let startScreen = (
-      <div className="Screen green">
-        <audio src={Song} controls={false} autoPlay />
-        <span>{start}</span>
-      </div>
-    );
-
-    let restScreen = (
-      <div className="Screen red">
-        <audio src={Song} controls={false} autoPlay />
-        <span>{rest}</span>
-      </div>
-    );
-
-    let workoutScreen = isResting ? restScreen : startScreen;
     return (
-      <div className="App">{isSettings ? settingScreen : workoutScreen}</div>
+      <div className="App">
+        {isSettings ? (
+          settingScreen
+        ) : (
+          <Timer workoutTime={workoutTime} restTime={restTime} />
+        )}
+      </div>
     );
   }
 }
---
+++ src/timer.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import "./App.css";
+import Song from "./fire_bow.mp3";
+
+class App extends Component {
+  state = {
+    start: 1,
+    rest: 1,
+    isResting: false
+  };
+
+  componentDidMount = () => {
+    this.startStart();
+  };
+
+  startStart = () => {
+    this.timerInterval = setInterval(() => {
+      console.log("Start");
+      this.setState({ start: this.state.start + 1 }, () => {
+        if (this.state.start === parseInt(this.props.workoutTime) + 1) {
+          console.log("Claer Start");
+          clearInterval(this.timerInterval);
+          this.setState({ isResting: true, start: 1 });
+          this.startRest();
+        }
+      });
+    }, 1000);
+  };
+
+  startRest = () => {
+    this.restInterval = setInterval(() => {
+      console.log("Rest");
+      this.setState({ rest: this.state.rest + 1 }, () => {
+        if (this.state.rest === parseInt(this.props.restTime) + 1) {
+          console.log("Claer Rest");
+          clearInterval(this.restInterval);
+          this.setState({ isResting: false, rest: 1 });
+          this.startStart();
+        }
+      });
+    }, 1000);
+  };
+
+  render() {
+    let { start, rest, isResting } = this.state;
+
+    let startScreen = (
+      <div className="Screen green">
+        <audio src={Song} controls={false} autoPlay />
+        <span>{start}</span>
+      </div>
+    );
+
+    let restScreen = (
+      <div className="Screen red">
+        <audio src={Song} controls={false} autoPlay />
+        <span>{rest}</span>
+      </div>
+    );
+
+    let workoutScreen = isResting ? restScreen : startScreen;
+    return <div className="App">{workoutScreen}</div>;
+  }
+}
+
+export default App;
